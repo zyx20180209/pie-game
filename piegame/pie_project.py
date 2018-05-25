6138b23424e47d1d17d1b2e9f7227511051e3eb5
zyx20180209 <36314398+zyx20180209@users.noreply.github.com>
2018-05-25 11:23:00 +0800

    Add files via upload

--- piegame/pie_project.py
+++ piegame/pie_project.py
@@ -4,16 +4,31 @@ import time
 from pygame.locals import *
 pygame.init()
 
-screen = pygame.display.set_mode((300, 600))
+
+def draw_block(pos_x, pos_y):
+    color = 227,222,0
+    shade_color = 150,150,0
+    bright_color = 255,255,147
+    width = 0
+    width_side = 6
+    pos = pos_x+2, pos_y+2, 26, 26
+    pygame.draw.rect(screen, color, pos, width)
+    pygame.draw.line(screen, bright_color, (pos_x,pos_y), (pos_x + 30,pos_y), 4)
+    pygame.draw.line(screen, bright_color, (pos_x,pos_y), (pos_x,pos_y + 30), 4)
+    pygame.draw.line(screen, shade_color, (pos_x + 30,pos_y), (pos_x + 30,pos_y+30), 5)
+    pygame.draw.line(screen, shade_color, (pos_x,pos_y + 30), (pos_x + 30,pos_y+30), 5)
+
+    
+screen = pygame.display.set_mode((340, 680))
 
 pygame.display.set_caption("Drawing Rectangle")
 pos_x = 0
 pos_y = 0
 vel_x = 0
-vel_y = 30
+vel_y = 34
 color = 255,255,0
-box0 = [600]
-box1 = [600]
+box0 = [680]
+box1 = [680]
 
 while True:
     for event in pygame.event.get():
@@ -25,31 +40,28 @@ while True:
     pos_x += vel_x
     pos_y += vel_y
       
-    if pos_x == 0 and pos_y == box0[-1]-30:
-        box0 += [pos_y]
+    if pos_x == 0 and pos_y == box0[-1]-34:
+        box0 += [box0[-1]-34]
         a = random.randint(0,1)
-        pos_x = 30 * a
+        pos_x = 34 * a
         pos_y = 0
         
-    if pos_x == 30 and pos_y == box1[-1]-30:
-        box1 += [pos_y]
+    if pos_x == 34 and pos_y == box1[-1]-34:
+        box1 += [box1[-1]-34]
         a = random.randint(0,1)
-        pos_x = 30 * a
+        pos_x = 34 * a
         pos_y = 0
     
     width = 0
     pos = pos_x, pos_y, 30, 30
-    pygame.draw.rect(screen,color,pos,width)
-
+    draw_block(pos_x, pos_y)
     if box0 != []:
         for i in range(len(box0)):
-            pos_b0 = 0, box0[i], 30, 30
-            pygame.draw.rect(screen,color,pos_b0,width)
+            draw_block(0, box0[i])
                         
     if box1 != []:
         for i in range(len(box1)):
-            pos_b1 = 30, box1[i], 30, 30
-            pygame.draw.rect(screen,color,pos_b1,width)
+            draw_block(34, box1[i])
     
     time.sleep(0.1)
     pygame.display.update()
@@ -57,3 +69,6 @@ while True:
     if box0[-1] == 0 or box1[-1] == 0:
         sys.exit()
 
+
+
+    
